7ab8c98410d06e1b8026491ab9db51f0310e6b9a
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2020-12-03 00:27:39 +0100

    Update products.ts

--- src/app/products.ts
+++ src/app/products.ts
@@ -18,11 +18,10 @@ export const products = [
     description: '',
     clicked: false
   }
+    {
+    name: 'Android Phone',
+    price: 1000,
+    description: 'Android ecosystem is managed and maintained by Google',
+    clicked: false
+  }
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
